bd3e8f03da1749fcbb4e22349d632c5f00530eeb
IgnacioDevo <pachecoignaciodev@gmail.com>
2022-09-12 13:51:50 -0300

    JS

--- Booklet/app.js
+++ Booklet/app.js
@@ -1,7 +1,7 @@
+ 
+ /* Ejercicio 1  */
 
-/* Ejercicio 1 
-
-let nombre = "Homero";
+/* let nombre = "Homero";
 
 let apellido = "Simpson";
 
@@ -9,5 +9,39 @@ let edad = 39;
 
 console.log(nombre);
 console.log(apellido);
-console.log(edad); */
+console.log(edad);  */
+
+/* Ejercicio 2 */
+
+/* const Alemania = "Berlin";
+
+const Argentina = "Buenos Aires";
+
+const Chile = "Satiago de Chile";
+
+const EEUU = "New York";
+
+const Brasil = "Rio";
+
+console.log(Alemania);
+console.log(Argentina);
+console.log(Chile); 
+console.log(EEUU);
+console.log(Brasil); */
+
+
+/* Ejercicio 3 */
+
+let carnet = "Carnet: "
+
+let name = "Ignacio Pacheco, ";
+
+let edad = 18 ;
+
+let licencia = " Expira en 2025";
+
+let LicenciaConducir = [ carnet + name  + edad + licencia];
+
+console.log(LicenciaConducir);
+
 
